610ed6033b34b35fd49d922c7117b07eba05173b
sebas42069 <s.a.lehikoinen@students.uu.nl>
2021-10-10 11:46:27 +0200

    gdfas

---
+++ ShapeDrawing/Converter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShapeDrawing
+{
+    abstract class Converter
+    {
+        public abstract string Convert(List<Shape> shapes);
+    }
+}
---
+++ ShapeDrawing/SVGConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShapeDrawing
+{
+    class SVGConverter : Converter
+    {
+        public override string Convert(List<Shape> shapes)
+        {
+            foreach (Shape shape in shapes)
+            {
+                switch (shape)
+                {
+                    case Circle:
+                        
+                        break;
+                    case Rectangle:
+                        break;
+                    case Star:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
